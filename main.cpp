aaeef570c2c8cf9d89cff48c2d582bca7d32a0d0
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2024-04-28 18:35:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 int main()
 {
 
   // Summer positions (Beijers Park, Malmö)
   // 55°36'38.1"N 13°03'01.1"E
-  const double summer_pos_long = 55.36381
-  const double summer_pos_lat = 13.03011
+  private const double summershadow_summer_pos_long = 55.36381
+  private const double summershadow_summer_pos_lat = 13.03011
 
   // Current position
-  double current_long;
-  double current_lat;
+  private double summer_shadow_current_long;
+  private double summer_shadow_current_lat;
 
+  time = system_clock::now();
+  
+  
+  
   return 0
 }
