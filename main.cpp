af44d6e2425e97a2df9143c5200893fcbca68d3a
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2024-04-28 17:35:32 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  // Summer positions (Beijers Park, Malmö)
+  const string summer_pos_long;
+  const string summer_pos_lat;
+
+  // Current position
+  string current_long;
+  string current_lat;
+
+  return 0
+}
